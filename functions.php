043fc28882ed6b210879ca93cc266ccf50fec23b
Simon <simon.delmotte@staminic.com>
2020-05-08 14:25:02 +0200

    Copie des fichiers du thème

--- functions.php
+++ functions.php
@@ -1,7 +1,33 @@
 <?php
-
-/* Chargement de la feuille du style du theme parent */
-add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
-function wpchild_enqueue_styles(){
-  wp_enqueue_style( 'wpm-understrap-style', get_template_directory_uri() . '/style.css' );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
+
+function understrap_remove_scripts() {
+    wp_dequeue_style( 'understrap-styles' );
+    wp_deregister_style( 'understrap-styles' );
+
+    wp_dequeue_script( 'understrap-scripts' );
+    wp_deregister_script( 'understrap-scripts' );
+
+    // Removes the parent themes stylesheet and scripts from inc/enqueue.php
+}
+add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+
+	// Get the theme data
+	$the_theme = wp_get_theme();
+    wp_enqueue_style( 'handisoutien-styles', get_stylesheet_directory_uri() . '/css/handisoutien.css', array(), $the_theme->get( 'Version' ) );
+    wp_enqueue_script( 'jquery');
+    wp_enqueue_script( 'handisoutien-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+}
+
+function add_child_theme_textdomain() {
+    load_child_theme_textdomain( 'handisoutien', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
