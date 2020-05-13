c83db47a3c158b5d5301b1c8167e123109cbc013
Simon <simon.delmotte@staminic.com>
2020-05-13 20:50:13 +0200

    Intégration du template

--- functions.php
+++ functions.php
@@ -82,3 +82,21 @@ function custom_ocs_sidebar_args( $args, $sidebar_id, $sidebar_data ) {
 	$args['after_title'] = '</h3>';
   return $args;
 }
+
+add_filter( 'wp_trim_excerpt', 'understrap_all_excerpts_get_more_link' );
+if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
+	/**
+	 * Adds a custom read more link to all excerpts, manually or automatically generated
+	 *
+	 * @param string $post_excerpt Posts's excerpt.
+	 *
+	 * @return string
+	 */
+	function understrap_all_excerpts_get_more_link( $post_excerpt ) {
+		if ( ! is_admin() ) {
+			$post_excerpt = $post_excerpt . ' [...]<p><a class="btn btn-link understrap-read-more-link" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More...',
+			'understrap' ) . '</a></p>';
+		}
+		return $post_excerpt;
+	}
+}
